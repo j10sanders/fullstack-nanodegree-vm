33c45fe8a0398ffee7d85646aa61e646c709fb0f
j10sanders <jonsandersss@gmail.com>
2016-03-14 20:01:06 -0400

    delete players works

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -17,7 +17,11 @@ def deleteMatches():
 
 def deletePlayers():
     """Remove all the player records from the database."""
-
+    conn = connect()
+    c = conn.cursor()
+    c.execute("delete from players *;")
+    conn.commit()
+    conn.close()
 
 def countPlayers():
     """Returns the number of players currently registered."""
